52d293d0de4e5e9112d1431971b8c86fdeb01c94
ist188123 <paulorosario@sapo.pt>
2019-04-14 17:30:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
 const Discord = require("discord.js");
-var resposta = require('./resposta.json');
+
 const client = new Discord.Client();
-var jsonData = require('./raidspn.json');
-const prefix = "+";
+
 
 
 
 
 
 client.on("message", async (msg) => {
-    let mewtwo = "";
-    let cor = "0x00AE86";
-    let mais = "";
-    let autor = ""
-    let quantidade = 0;
-    let obs = "";
-    var regra = "Mod"
-    let pkmraid = "Desconhecido";
-    let adicional = "";
-    
+   
 
         if (msg.content.startsWith("!")) {
 
