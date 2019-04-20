c499e1b7dca59d4e38022a78abdab6b7ff2a6c82
ist188123 <paulorosario@sapo.pt>
2019-04-20 13:02:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,10 +13,18 @@ bot.on('message', message => {
 
     if (msg === '/Q') {
         message.channel.send('Bem vindo as Quests.\n para introduzir uma Quest escolha a opção\n'
-          +'1 - EVOLUIR - OVOS\n'
-          +'2 - BATALHAS GINÁSIO - RAIDS\n'
-          +'3 - LANCAMENTOS\n'
-          +'4 - APANHAR EVOLUIR\n');
+          +'1  OVOS\n'
+          +'2  BATALHAS GINÁSIO/RAIDS\n'
+          +'3  LANCAMENTOS\n'
+          +'4  APANHAR/EVOLUIR/OUTROS\n');
+        return;
+    }
+    
+    if (msg === '11') {
+        message.channel.send('OVOS\n'
+          +'111 UM OVO\n'
+          +'112 TRES OVOS\n'
+          +'113 CINCO OVOS\n');
         return;
     }
 
