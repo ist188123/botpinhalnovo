f824eecdcb499cf324df4aab6bd56821a0545206
ist188123 <paulorosario@sapo.pt>
2019-04-24 16:43:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,53 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
+const Discord = require('discord.js')
+const client = new Discord.Client()
 
-
-const prefix = '/q';
-
-bot.on('message', message => {
-
-    
-
-    if (message === '#5') {
-        message.channel.send('Bem vindo as Quests.\n para introduzir uma Quest escolha a opção\n'
-          +'1  OVOS\n'
-          +'2  BATALHAS GINÁSIO/RAIDS\n'
-          +'3  LANCAMENTOS\n'
-          +'4  APANHAR/EVOLUIR/OUTROS\n');
-        return;
+client.on('message', (receivedMessage) => {
+    if (receivedMessage.author == client.user) { // Prevent bot from responding to its own messages
+        return
     }
     
-    
+    if (receivedMessage.content.startsWith("!")) {
+        processCommand(receivedMessage)
+    }
+})
 
-});
+function processCommand(receivedMessage) {
+    let fullCommand = receivedMessage.content.substr(1) // Remove the leading exclamation mark
+    let splitCommand = fullCommand.split(" ") // Split the message up in to pieces for each space
+    let primaryCommand = splitCommand[0] // The first word directly after the exclamation is the command
+    let arguments = splitCommand.slice(1) // All other words are arguments/parameters/options for the command
 
+    console.log("Command received: " + primaryCommand)
+    console.log("Arguments: " + arguments) // There may not be any arguments
 
+    if (primaryCommand == "help") {
+        helpCommand(arguments, receivedMessage)
+    } else if (primaryCommand == "multiply") {
+        multiplyCommand(arguments, receivedMessage)
+    } else {
+        receivedMessage.channel.send("I don't understand the command. Try `!help` or `!multiply`")
+    }
+}
 
-bot.login(process.env.BOT_TOKEN);
+function helpCommand(arguments, receivedMessage) {
+    if (arguments.length > 0) {
+        receivedMessage.channel.send("It looks like you might need help with " + arguments)
+    } else {
+        receivedMessage.channel.send("I'm not sure what you need help with. Try `!help [topic]`")
+    }
+}
+
+function multiplyCommand(arguments, receivedMessage) {
+    if (arguments.length < 2) {
+        receivedMessage.channel.send("Not enough values to multiply. Try `!multiply 2 4 10` or `!multiply 5.2 7`")
+        return
+    }
+    let product = 1 
+    arguments.forEach((value) => {
+        product = product * parseFloat(value)
+    })
+    receivedMessage.channel.send("The product of " + arguments + " multiplied together is: " + product.toString())
+}
+
+client.login(process.env.BOT_TOKEN);
 
