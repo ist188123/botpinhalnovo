46b99411bc7d68e9a0a7a03ba4caf4d7f029a21b
ist188123 <paulorosario@sapo.pt>
2019-05-01 16:21:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client(); 
+
 let totalSeconds = (client.uptime / 1000);
 let days = Math.floor(totalSeconds / 86400);
 let hours = Math.floor(totalSeconds / 3600);
@@ -10,8 +11,8 @@ let seconds = totalSeconds % 60;
 let uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
 client.on("message", (message) => {
 if(message.content == "!ping"){ // Check if content of message is "!ping"
-	
-	
+	message.channel.send(client.uptime);
+	message.channel.send(days);
 	message.channel.send(uptime);
 	
 		message.channel.send("pong!"); // Call .send() on the channel object the message was sent in
