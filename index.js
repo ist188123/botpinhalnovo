dad63d47c6e09e2f75b5fe7fbf0eeb48609d6709
ist188123 <paulorosario@sapo.pt>
2019-05-01 23:47:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -22,19 +22,8 @@ var add_minutes =  function (dt, minutes) {
 
 
 
-client.on("message", (message) => {
-if(message.content == "!ping"){ // Check if content of message is "!ping"
+function informa_raid(message){
 	
-	message.channel.send("Data : "+dia(new Date()));
-       message.channel.send("Ovo : "+add_minutes(new Date(), -40).toString());
-       message.channel.send("Abre : "+add_minutes(new Date(), 5).toString());
-       message.channel.send("Termina : "+add_minutes(new Date(), 50).toString());
-
-		
-	
-
-	
-	//--- mensagem raid --
 	message.channel.send({embed: {
     color: 3447003,
     author: {
@@ -64,9 +53,25 @@ if(message.content == "!ping"){ // Check if content of message is "!ping"
     }
   }
 });
+	
+}
 
 
-//-- fim mensagem raid-- 	
+
+
+
+client.on("message", (message) => {
+if(message.content == "!ping"){ // Check if content of message is "!ping"
+	
+	message.channel.send("Data : "+dia(new Date()));
+       message.channel.send("Ovo : "+add_minutes(new Date(), -40).toString());
+       message.channel.send("Abre : "+add_minutes(new Date(), 5).toString());
+       message.channel.send("Termina : "+add_minutes(new Date(), 50).toString());
+
+		
+	informa_raid(message);
+
+		
 	
 	
 	
