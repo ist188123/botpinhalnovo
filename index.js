33f62bdf80652e7a8f39cbaa0a36b7a638680e23
ist188123 <paulorosario@sapo.pt>
2019-05-01 17:20:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const Discord = require("discord.js");
 const client = new Discord.Client(); 
-var moment = require('moment');
+
 client.on("message", (message) => {
 if(message.content == "!ping"){ // Check if content of message is "!ping"
+	
+	message.channel.send(new Date(Date.now()).toLocaleString());
 		message.channel.send("pong!"); // Call .send() on the channel object the message was sent in
 	}
 });
