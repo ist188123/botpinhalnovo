2f2f456d3a41e96723747e3f1c09b414a33f03d9
ist188123 <paulorosario@sapo.pt>
2019-05-01 23:38:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -29,10 +29,62 @@ if(message.content == "!ping"){ // Check if content of message is "!ping"
        message.channel.send("Ovo : "+add_minutes(new Date(), -40).toString());
        message.channel.send("Abre : "+add_minutes(new Date(), 5).toString());
        message.channel.send("Termina : "+add_minutes(new Date(), 50).toString());
-	
-	
+
 		
-	}
+	
+
+	
+	//--- mensagem raid --
+	message.channel.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "This is an embed",
+    url: "http://google.com",
+    description: "This is a test embed to showcase what they look like and what they can do.",
+    fields: [{
+        name: "Fields",
+        value: "They can have different fields with small headlines."
+      },
+      {
+        name: "Masked links",
+        value: "You can put [masked links](http://google.com) inside of rich embeds."
+      },
+      {
+        name: "Markdown",
+        value: "You can put all the *usual* **__Markdown__** inside of them."
+      }
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© Example"
+    }
+  }
+});
+
+
+//-- fim mensagem raid-- 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}//fim ping
+	
 });
 client.login(process.env.BOT_TOKEN);
 
