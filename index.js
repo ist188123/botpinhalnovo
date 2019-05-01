779360576bd14163e8550dea75145b523692aff5
ist188123 <paulorosario@sapo.pt>
2019-05-01 16:19:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ client.on("message", (message) => {
 if(message.content == "!ping"){ // Check if content of message is "!ping"
 	
 	
-	
+	message.channel.send(uptime);
 	
 		message.channel.send("pong!"); // Call .send() on the channel object the message was sent in
 	}
