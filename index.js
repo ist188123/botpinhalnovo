d656255299d7edd7b8dce297efc1689f23e6c743
ist188123 <paulorosario@sapo.pt>
2019-05-01 23:29:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 const Discord = require("discord.js");
 const client = new Discord.Client(); 
 
+
+
+var dia =  function (dt) {
+    var d =  new Date(dt.getTime()),
+    dformat = [d.getDate(),
+        d.getMonth()+1,
+           d.getFullYear()].join('-');
+
+           return dformat;
+}
+
+var add_minutes =  function (dt, minutes) {
+    var d =  new Date(dt.getTime() + minutes*60000),
+    dformat =  [d.getHours(),
+           d.getMinutes()].join(':');
+
+           return dformat;
+}
+
+
+
 client.on("message", (message) => {
 if(message.content == "!ping"){ // Check if content of message is "!ping"
 	
-	var d = new Date,
-dformat = [d.getMonth()+1,
-       d.getDate(),
-       d.getFullYear()].join('-')+' '+
-      [d.getHours(),
-       d.getMinutes()].join(':');
+	message.channel.send("Data : "+dia(new Date()));
+       message.channel.send("Ovo : "+add_minutes(new Date(), -40).toString());
+       message.channel.send("Abre : "+add_minutes(new Date(), 5).toString());
+       message.channel.send("Termina : "+add_minutes(new Date(), 50).toString());
 	
-	message.channel.send(dformat.toLocaleString());
-		message.channel.send("pong!"); // Call .send() on the channel object the message was sent in
+	
+		
 	}
 });
 client.login(process.env.BOT_TOKEN);
