9b5b989f75f5a8f99c66b1fe0b5f36978bdb817e
ist188123 <paulorosario@sapo.pt>
2019-05-01 16:54:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const Discord = require("discord.js");
 const client = new Discord.Client(); 
-const data =require('date-utils');
+let date = require('date-and-time');
 
 let now = new Date();
 
 client.on("message", (message) => {
 if(message.content == "!ping"){ // Check if content of message is "!ping"
 	
-	
+	message.channel.send(now); 
 
 		message.channel.send("pong!"); // Call .send() on the channel object the message was sent in
 	}
