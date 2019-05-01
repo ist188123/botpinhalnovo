1c56789cfb32c52f32e963377aa2102e0fd8cdd5
ist188123 <paulorosario@sapo.pt>
2019-05-01 22:46:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,10 +7,9 @@ if(message.content == "!ping"){ // Check if content of message is "!ping"
 	var d = new Date,
 dformat = [d.getMonth()+1,
        d.getDate(),
-       d.getFullYear()].join('/')+' '+
+       d.getFullYear()].join('-')+' '+
       [d.getHours(),
-       d.getMinutes(),
-       d.getSeconds()].join(':');
+       d.getMinutes()].join(':');
 	
 	message.channel.send(dformat.toLocaleString());
 		message.channel.send("pong!"); // Call .send() on the channel object the message was sent in
