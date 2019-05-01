e8943366bdb4afef84667adf9159bc8e1fd52d87
ist188123 <paulorosario@sapo.pt>
2019-05-01 16:49:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ let now = new Date();
 client.on("message", (message) => {
 if(message.content == "!ping"){ // Check if content of message is "!ping"
 	
-	message.channel.send(now);
-	message.channel.send(Date.today());
+	
 
 		message.channel.send("pong!"); // Call .send() on the channel object the message was sent in
 	}
