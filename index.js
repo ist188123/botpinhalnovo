7b88a9f006fa9194fdf0403efd32ee032884d250
ist188123 <paulorosario@sapo.pt>
2019-05-08 09:32:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-client.on('message', msg => {
-  if (msg.content === 'ping') {
+client.on('message', message => {
+  if (message.content === 'ping') {
     
     
     
