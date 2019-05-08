5be9a75c3dcda66bc0b72fc9aa24866233af24c9
ist188123 <paulorosario@sapo.pt>
2019-05-08 09:11:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 client.on('message', msg => {
   if (msg.content === 'ping') {
-    msg.reply('pong')
+    
+    
+    
+    
+    msg.channel.send({embed: {
+  color: 15158332,
+  description: "A very simple Embed!"
+}});
+    
+    
+    
+    
   }
 })
 client.login(process.env.BOT_TOKEN)
