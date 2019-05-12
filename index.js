6015fc8be90f2f265fff6a6c0c820ac9ab2a5c35
ist188123 <paulorosario@sapo.pt>
2019-05-13 00:34:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,22 +3,14 @@ const client = new Discord.Client()
 client.on('message', message => {
   if (message.content === '!') {
     
-    var msg=message.content.split('!')
     
-    var myInfo = new discord.RichEmbed()
-  .setTitle("Hound")
-  .addField("Name", "Hound")
-  .addField("Age", "12")
-  .addField("Description", "Im good at siege, I stream occasionally and ya")
-  .setColor("#020B0C")
-  .attachFile('./img/Alakazam.png')
-  .setImage('attachment://Alakazam.png');
-message.channel.sendEmbed(myInfo);
  
- 
+  const exampleEmbed = new Discord.RichEmbed()
+	.setTitle('Some title')
+	.attachFiles(['./img/Alakazam.png'])
+	.setImage('attachment://Alakazam.png');  
     
-    
-    
+    message.channel.sendEmbed(exampleEmbed);
   }
 })
 client.login(process.env.BOT_TOKEN)
