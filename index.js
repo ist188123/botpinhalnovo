e0adce7e5df680f258b481f492846366a1ddc9ca
ist188123 <paulorosario@sapo.pt>
2019-05-13 11:17:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,19 +7,13 @@ client.on('message', message => {
   var imagem="./img/Articune.png"
   var attimage="Articune.png"
  //---------
-  var myInfo = new discord.RichEmbed()
-  .setTitle("Hound")
-  .addField("Name", "Hound")
-  .addField("Age", "12")
-  .addField("Description", "Im good at siege, I stream occasionally and ya")
-  .setColor("#020B0C")
-  .attachFile('./img/Articune.png')
-  .setImage('attachment://Articune.png');
-message.channel.send(myInfo);
   
-  
-  
-  
+  const exampleEmbed = new Discord.RichEmbed()
+	.setTitle('Some title')
+	.attachFiles(['./img/Articune.png'])
+	.setImage('attachment://Articune.png');
+
+channel.send(exampleEmbed);
   
   
 	  
