c9f970f8775b2d993588d68d266cdd69459d66a8
ist188123 <paulorosario@sapo.pt>
2019-05-13 10:58:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,48 +6,13 @@ client.on('message', message => {
     var text = message.content.substring(1);
  
  
-    const embed = {
-  "title": "A Title" ,
-  "color": 0xF96221,
- "attachFile" : "./img/Articune.png",
-  "thumbnail": {
-	 
-    "url": "attachment://Articune.png"
-  },
-  "fields": [
-    {
-      "name": "Field 1:",
-      "value": "One",
-      "inline": false
-    },
-    {
-      "name": "Field 2:",
-      "value": "Two",
-      "inline": true,
-    },
-    {
-      "name":"Field 3:",
-      "value":"Three",
-      "inline": true
-    },
-  ],
-  "footer": {
-    "text":"Footer text"
-  }
-};
-	  
-	  
-	  
-	message.channel.send({
-  embed,
-  files: [{
-    attachment:'./img/Articune.png',
-    name:'Articune.png'
-  }]
-});
   
     
-	  
+	 message.channel.send('Message that goes above image', {
+            files: [
+                "./img/Articune.png"
+            ]
+        });  
 	  
 	  
 	  
