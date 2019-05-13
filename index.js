fd4f586d7ae68839c70663304758d7c8608805ed
ist188123 <paulorosario@sapo.pt>
2019-05-13 11:40:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const client = new Discord.Client()
 client.on('message', message => {
   if (message.content === '!') {
     
-    var text = message.content.substring(1);
-  var imagem="./img/Articune.png"
-  var attimage="Articune.png"
+ 
+  
  //---------
   
   
