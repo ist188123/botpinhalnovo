802a77901b0b74fdee5fa0baa8955aa4b83c0e4f
ist188123 <paulorosario@sapo.pt>
2019-05-23 16:26:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
- 
+
+const http = require('http');
+
 client.on("ready", () => {
   console.log("I am ready!");
 });
  
 client.on("message", (message) => {
   if (message.content.startsWith("ping")) {
+   
+    var endereco='http://pnraidspn.atwebpages.com/teste.php'
+   
     message.channel.send("pong!");
   }
 });
