944b89cdab5a6811fc2e734b521b4a8815e4a12e
ist188123 <paulorosario@sapo.pt>
2019-05-23 17:46:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,32 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
+  
+  if (msg.content.startsWith('!')) {
+
+
+
+      var dmsg = msg.content.substring(1);
+
+
+      let cod = dmsg.split(" ")[0];
+
+      let pokestop = dmsg.split(" ").slice(1).join(" ");
+      let quest = "";
+      let missao = "";
+      let questimagem = "";
+  
+  
+  
+  
     msg.reply('Pong!');
+
+    
+    
   }
+  
+  
+  
+  
 });
 client.login(process.env.BOT_TOKEN);
