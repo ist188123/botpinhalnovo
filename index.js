47465f00ef9aa7b910015f64a0a976033f6f1165
ist188123 <paulorosario@sapo.pt>
2019-05-23 16:29:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,45 @@ client.on("ready", () => {
 });
  
 client.on("message", (message) => {
+  
+  
   if (message.content.startsWith("ping")) {
    
     var endereco='http://pnraidspn.atwebpages.com/teste.php'
    
+    
+    let req = http.get(endereco, function(res) {
+	let data = '',
+		json_data;
+
+	res.on('data', function(stream) {
+		data += stream;
+	});
+	res.on('end', function() {
+		json_data = JSON.parse(data);
+
+        // will output a Javascript object
+        for ( a=0; a<json_data.length;a++){
+            //console.log(json_data[a].boss);
+        }
+		
+	});
+});
+
+    
+    
+    
+    
+    
+    
+    
     message.channel.send("pong!");
   }
+  
+  
+  
+  
+  
+  
 });
 client.login(process.env.BOT_TOKEN);
