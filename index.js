ce8d06a99caa73ee9b7fb74cd96449fd4cd58292
ist188123 <paulorosario@sapo.pt>
2019-05-23 17:43:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,74 +1,13 @@
-const Discord = require("discord.js");
-var resposta = require('./resposta.json');
+const Discord = require('discord.js');
 const client = new Discord.Client();
-var jsonData = require('./raidspn.json');
-const prefix = "+";
 
-
-
-//console.log(tamanhoFicheiro)
-
-
-
-
-
-
-
-
-
-client.on("message", async (msg) => {
-  
-//--------------------------
-  //----criar canal ---
-
-  if (msg.channel.name == 'professor-boss') {
-
-    if (msg.content.startsWith('!')) {
-
-
-
-      var dmsg = msg.content.substring(1);
-
-
-      let cod = dmsg.split(" ")[0];
-
-      let pokestop = dmsg.split(" ").slice(1).join(" ");
-      let quest = "";
-      let missao = "";
-      let questimagem = "";
-      
-
-      msg.sendMessage("pong!");
-
-
-
-
-    }//fim regras
-
-
-
-
-
-
-
-
-
-
-  }//fim if
-
-  //fim criar canal----
-
-
-
-
-
-
-
-
-
-
-  //--- fim teste ----
-
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
 client.login(process.env.BOT_TOKEN);
